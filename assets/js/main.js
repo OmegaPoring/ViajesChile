c1de65e309c0eac34a9b7e64f8d5b67a73fe726a
Giovanni Inarejo <OmegaPoring@hotmail.com>
2024-04-04 00:52:11 -0300

    Se cambia script de smooth scroll de HTML a JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,25 @@ if (intScroll>=perfChange){
     bgNavBar.classList.remove("bg-black")
 }
 }
+
+//Funcion Smooth Scroll//
+
+$(document).ready(function(){
+        
+    $("a").on('click', function(event) {
+        
+        if (this.hash !== "") {
+        
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+            window.location.hash = hash;
+        });
+        }
+    });
+});
